06e3e287f10b31507e0630a796c5489b3d2d0489
dyakovmaksim <pochta3478@mail.ru>
2025-01-18 18:46:35 +0500

    header

--- client/components/AppHeader.vue
+++ client/components/AppHeader.vue
@@ -1,3 +1,11 @@
 <template>
-  <header>header</header>
+  <header class="bg-[var(--blue-dark)] flex justify-between items-center px-4 py-2">
+    <img src="public/logo.svg">
+    <div class="bg-white w-[40%]  flex justify-between rounded-lg p-1">
+      <input type="text" class="border-none outline-none w-[90%] rounded" placeholder="Поиск">
+      <button class="bg-[var(--blue)] text-xs text-white rounded p-1"><Icon name="simple-line-icons:magnifier" /></button>
+    </div>
+    <button class="bg-[var(--blue)] text-white font-bold rounded p-1">Заявки</button>
+
+  </header>
 </template>
