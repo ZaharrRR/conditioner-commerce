6c49d6219e7981fd1efc46676c9bc8288b502b71
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2025-01-18 18:05:00 +0500

    button component

---
+++ client/pages/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtLayout name="page-layout">
+    <h1>test page</h1>
+
+    <div class="buttons flex gap-3">
+      <UButton size="medium" color="blue" variant="solid">Solid Button</UButton>
+      <UButton size="small" color="blue-dark" variant="outline"
+        >Outline Button</UButton
+      >
+      <UButton size="small" color="blue-accent" variant="link"
+        >Link Button</UButton
+      >
+      <UButton size="medium" color="blue-lightest" variant="solid" disabled
+        >Disabled Button</UButton
+      >
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import UButton from "~/components/UI/UButton.vue";
+</script>
+
+<style scoped></style>
