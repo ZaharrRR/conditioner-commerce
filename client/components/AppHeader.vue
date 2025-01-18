4b36fd4ace76641577a9c64eb9aef54d61fcbae5
dyakovmaksim <pochta3478@mail.ru>
2025-01-19 00:13:09 +0500

    update main

--- client/components/AppHeader.vue
+++ client/components/AppHeader.vue
@@ -1,20 +1,24 @@
-
 <template>
-  <header>
-    <div class="bg-[var(--blue-dark)] flex justify-between items-center px-4 py-2">
-      <img src="public/logo.svg">
-      <div class="bg-white w-[40%]  flex justify-between rounded-lg p-1">
-        <input type="text" class="border-none outline-none w-[90%] rounded" placeholder="Поиск">
-        <button class="bg-[var(--blue)] text-xs text-white rounded p-1"><Icon name="simple-line-icons:magnifier" /></button>
+  <header class="bg-[var(--blue-dark)]">
+    <div class="flex justify-between items-center px-6 py-6 w-[1280px] mx-auto">
+      <img src="public/logo.svg" />
+      <div class="bg-white w-[40%] flex justify-between rounded-lg p-1">
+        <input
+          type="text"
+          class="border-none outline-none w-[90%] rounded"
+          placeholder="Поиск"
+        />
+        <button class="bg-[var(--blue)] text-xs text-white rounded p-1">
+          <Icon name="simple-line-icons:magnifier" />
+        </button>
       </div>
-      <UButton size="small" color="blue" variant="solid">Заявки</UButton>
     </div>
-    <div class="bg-[var(--blue)]">
-      <nav class="flex gap-5 p-3">
-        <UButton size="small" color="blue-dark" variant="solid">Товары</UButton>
-        <UButton size="small" color="blue-dark" variant="solid">Услуги</UButton>
-        <UButton size="small" color="blue-dark" variant="solid">Доставка</UButton>
-        <UButton size="small" color="blue-dark" variant="solid">Контакты</UButton>
+    <div class="bg-[var(--blue)] text-white">
+      <nav class="flex gap-16 px-6 py-2 w-fit mx-auto">
+        <NuxtLink>Товары</NuxtLink>
+        <NuxtLink>Услуги</NuxtLink>
+        <NuxtLink>Доставка</NuxtLink>
+        <NuxtLink>Контакты</NuxtLink>
       </nav>
     </div>
   </header>
