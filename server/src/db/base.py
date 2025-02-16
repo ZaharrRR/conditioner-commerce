86c5a66276eb5471cc15ad6c5622fb022b620802
wtckk <nikita.shutnikov89@gmail.com>
2025-02-16 17:50:29 +0500

    add server default generate base class

--- server/src/db/base.py
+++ server/src/db/base.py
@@ -14,7 +14,8 @@ class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid4
+        UUID(as_uuid=True), primary_key=True, default=uuid4,
+        server_default=func.uuid_generate_v4()
     )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
