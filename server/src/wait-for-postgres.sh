993ab3f55d4c4256bf26d76d03d348006c93c090
wtckk <125646535+wtckk@users.noreply.github.com>
2025-04-02 09:09:11 +0500

    fix db

---
+++ server/src/wait-for-postgres.sh
@@ -0,0 +1,7 @@
+echo "Ожидание PostgreSQL (${POSTGRES_HOST}:${POSTGRES_PORT})..."
+
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+  sleep 1
+done
+
+echo "PostgreSQL готов!"
--- wait-for-postgres.sh
+++
@@ -1,7 +0,0 @@
-echo "⏳ Ожидание PostgreSQL (${POSTGRES_HOST}:${POSTGRES_PORT})..."
-
-while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
-  sleep 1
-done
-
-echo "✅ PostgreSQL готов!"
