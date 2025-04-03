bce18ec6c3a14921fbaacb51d718217ae6a4575f
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2025-04-03 14:55:34 +0500

    host url

--- client/api/http/index.js
+++ client/api/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export let adminApiKey = null;
 
-export const host = "http://localhost:3000";
+export const host = "http://backend:3000";
 
 export const setAdminApiKey = (key) => {
   adminApiKey = key;
